44a794df26236ec6beb87a2dc7c6323fac83ea87
Vigneshwar RK <rkvfcvemb@gmail.com>
2022-11-24 16:18:01 +0530

    rxjs observalbes initial commit

--- rxjs_observables/README.md
+++ rxjs_observables/README.md
@@ -1,4 +1,4 @@
-# LearnObservables
+# RxjsObservables
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- rxjs_observables/karma.conf.js
+++ rxjs_observables/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learn_observables'),
+      dir: require('path').join(__dirname, './coverage/rxjs_observables'),
       subdir: '.',
       reporters: [
         { type: 'html' },
